a2abbb9d0d3bdaacec5cad33d318433ebee6dcb5
Jmara <jmara.0923@gmail.com>
2024-11-01 19:37:17 +0200

    Initial files

---
+++ main.rb
@@ -0,0 +1,13 @@
+require_relative 'lib/knights_travails'
+
+knight = KnightTravails.new
+
+knight.knight_moves([0, 7], [7, 0])
+# You made it in 7 moves! Here's your path:
+# [0, 7]
+# [2, 6]
+# [4, 7]
+# [6, 6]
+# [7, 4]
+# [6, 2]
+# [7, 0]
